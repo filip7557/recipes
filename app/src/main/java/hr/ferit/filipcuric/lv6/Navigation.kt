087622b4c8573d0007d83f0e6cac4738909a898f
filip7557 <filip.curic@student.ferit.hr>
2023-12-05 20:37:59 +0100

    Optimize imports

--- app/src/main/java/hr/ferit/filipcuric/lv6/Navigation.kt
+++ app/src/main/java/hr/ferit/filipcuric/lv6/Navigation.kt
@@ -6,7 +6,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import androidx.navigation.navigation
 import hr.ferit.filipcuric.lv6.ui.RecipeDetailsScreen
 import hr.ferit.filipcuric.lv6.ui.RecipesScreen
 
