9907c216d1333217cc3272fad9c795106962de0b
filip7557 <filip.curic@student.ferit.hr>
2023-12-05 20:14:21 +0100

    Add navigation

--- app/src/main/java/hr/ferit/filipcuric/lv6/Navigation.kt
+++ app/src/main/java/hr/ferit/filipcuric/lv6/Navigation.kt
@@ -1,2 +1,52 @@
 package hr.ferit.filipcuric.lv6
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import androidx.navigation.navigation
+import hr.ferit.filipcuric.lv6.ui.RecipeDetailsScreen
+import hr.ferit.filipcuric.lv6.ui.RecipesScreen
+
+object Routes {
+    const val SCREEN_ALL_RECIPES = "recipeList"
+    const val SCREEN_RECIPE_DETAILS = "recipeDetails/{recipeId}"
+
+    fun getRecipeDetailsPath(recipeId: Int?) : String {
+        if (recipeId != null && recipeId != -1) {
+            return "recipeDetails/$recipeId"
+        }
+        return "recipeDetails/0"
+    }
+}
+
+@Composable
+fun NavigationController() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = Routes.SCREEN_ALL_RECIPES
+    ) {
+        composable(Routes.SCREEN_ALL_RECIPES) {
+            RecipesScreen(navigation = navController)
+        }
+        composable(
+            Routes.SCREEN_RECIPE_DETAILS,
+            arguments = listOf(
+                navArgument("recipeId") {
+                    type = NavType.IntType
+                }
+            )
+        ) {
+            backStackEntry ->
+                backStackEntry.arguments?.getInt("recipeId")?.let {
+                    RecipeDetailsScreen(
+                        navigation = navController,
+                        recipeId = it
+                    )
+                }
+        }
+    }
+}
